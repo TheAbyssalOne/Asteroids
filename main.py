1b465153120e59112e6e525aed0918b4f389ff80
TheAbyssalOne <gax1994@hotmail.com>
2024-08-26 00:21:58 +0200

    refactored the while True loop

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
-        screen.fill("black", rect=None, special_flags=0)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-
+        screen.fill("black")
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
     
